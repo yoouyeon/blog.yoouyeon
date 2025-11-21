56cb86addfda73a8dc30b572c092b359492d1864
yoouyeon <jyeon.yoon59@gmail.com>
2025-11-21 23:54:22 +0900

    refactor: CodeCopyButton 아이콘 렌더링 로직 단순화

--- src/components/CodeCopyButton.tsx
+++ src/components/CodeCopyButton.tsx
@@ -41,9 +41,13 @@ export default function CopyButton({ text }: CodeCopyButtonProps) {
       onClick={copy}
       className="absolute top-2.5 right-2.5 opacity-0 peer-hover:opacity-75 hover:opacity-100 text-black dark:text-white border border-black/15 dark:border-white/15 bg-white dark:bg-black rounded-md cursor-pointer"
     >
-      {copied && <CheckIcon className="h-5 w-5 m-2" />}
-      {failed && <ExclamationIcon className="h-5 w-5 m-2" />}
-      {!(copied && failed) && <CopyIcon className="h-5 w-5 m-2" />}
+      {copied ? (
+        <CheckIcon className="h-5 w-5 m-2" />
+      ) : failed ? (
+        <ExclamationIcon className="h-5 w-5 m-2" />
+      ) : (
+        <CopyIcon className="h-5 w-5 m-2" />
+      )}
     </button>
   );
 }
