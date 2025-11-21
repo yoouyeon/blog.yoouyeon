ea2c35a2ff6f7d45a8412bb2559abddba3d22444
yoouyeon <jyeon.yoon59@gmail.com>
2025-11-21 23:27:34 +0900

    refactor: CodeCopyButton 개선
    - 복사 실패 처리
    - 타이머 클린업 추가

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -153,3 +153,22 @@ export function UpIcon(props: SVGProps<SVGSVGElement>) {
     </svg>
   );
 }
+
+// Icon from Material Symbols by Google - https://github.com/google/material-design-icons/blob/master/LICENSE
+export function ExclamationIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      role="img"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M12 14q-.425 0-.712-.288T11 13V6q0-.425.288-.712T12 5t.713.288T13 6v7q0 .425-.288.713T12 14m0 5q-.425 0-.712-.288T11 18t.288-.712T12 17t.713.288T13 18t-.288.713T12 19"
+      />
+    </svg>
+  );
+}
