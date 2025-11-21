c6e78d7c04510947ef974ce32545201b136d690f
yoouyeon <jyeon.yoon59@gmail.com>
2025-11-21 22:43:37 +0900

    refactor: 사용하지 않는 블로그 상수 정리

--- src/components/BlogFooter.tsx
+++ src/components/BlogFooter.tsx
@@ -1,14 +1,11 @@
 import Link from "next/link";
-import { BLOG_PROFILE } from "@/config/blog";
 
 export default function BlogFooter() {
-  const { AUTHOR, LINKS } = BLOG_PROFILE;
-
   return (
     <footer className="flex flex-col mx-auto w-full max-w-screen-sm px-5 py-12">
       <div>
         <span>Made with ♥ by </span>
-        <Link href={LINKS.GITHUB}>{AUTHOR.EN}</Link>
+        <Link href="https://github.com/yoouyeon">yoouyeon</Link>
       </div>
       <span>© 2025</span>
     </footer>
