d6a18a6cb8869417942a77271fbdef895814627b
yoouyeon <jyeon.yoon59@gmail.com>
2025-11-21 23:33:39 +0900

    fix: CodeBlock 컴포넌트에 에러 처리 추가

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -7,22 +7,35 @@ type CodeBlockProps = {
 };
 
 async function CodeBlock({ codeblock }: CodeBlockProps) {
-  const highlighted = await highlight(codeblock, "github-from-css");
+  try {
+    const highlighted = await highlight(codeblock, "github-from-css");
 
-  return (
-    <figure className="font-mono my-6">
-      {highlighted.meta ? (
-        <figcaption className="text-sm mb-1 h-5">{highlighted.meta}</figcaption>
-      ) : null}
-      <div className="relative">
-        <Pre
-          className="not-prose px-4 py-3 rounded-md overflow-x-auto bg-black/5 dark:bg-[#212121] peer"
-          code={highlighted}
-        />
-        <CodeCopyButton text={highlighted.code} />
-      </div>
-    </figure>
-  );
+    return (
+      <figure className="font-mono my-6">
+        {highlighted.meta ? (
+          <figcaption className="text-sm mb-1">{highlighted.meta}</figcaption>
+        ) : null}
+        <div className="relative">
+          <Pre
+            className="not-prose px-4 py-3 rounded-md overflow-x-auto bg-black/5 dark:bg-[#212121] peer"
+            code={highlighted}
+          />
+          <CodeCopyButton text={highlighted.code} />
+        </div>
+      </figure>
+    );
+  } catch {
+    return (
+      <figure className="font-mono my-6">
+        <div className="relative">
+          <pre className="not-prose px-4 py-3 rounded-md overflow-x-auto bg-black/5 dark:bg-[#212121] peer">
+            <code>{codeblock.value}</code>
+          </pre>
+          <CodeCopyButton text={codeblock.value} />
+        </div>
+      </figure>
+    );
+  }
 }
 
 export default CodeBlock;
